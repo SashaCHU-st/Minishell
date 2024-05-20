306189720e0b6d6284c659587246303f94f1ae21
lenkras <yelenakras@gmail.com>
2024-05-20 18:22:04 +0300

    modify files/working v

--- minishell.h
+++ minishell.h
@@ -6,24 +6,25 @@
 /*   By: epolkhov <epolkhov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 12:53:11 by epolkhov          #+#    #+#             */
-/*   Updated: 2024/05/17 13:01:30 by epolkhov         ###   ########.fr       */
+/*   Updated: 2024/05/20 18:15:53 by epolkhov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdbool.h>
+# include <unistd.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
-typedef struct{
-	int size;
-	char **pipe_tok;
+typedef struct s_tok
+{
+	int		size;
+	char	**pipe_tok;
 }	t_tok;
 
 #endif

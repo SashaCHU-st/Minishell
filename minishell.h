e1bfc35135920b1f5304ecdfde94f54800c0dddd
Elena Polkhovski <epolkhov@c1r6p16.hive.fi>
2024-06-13 18:04:11 +0300

    modify heredoc file and other files

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: epolkhov <epolkhov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 12:53:11 by epolkhov          #+#    #+#             */
-/*   Updated: 2024/06/10 21:51:03 by epolkhov         ###   ########.fr       */
+/*   Updated: 2024/06/06 21:33:01 by epolkhov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ bool	has_unclosed_quotes(char *line);
 void	error_message(char *msg);
 
 char	**do_split(char const *s, char c);
-
 void	*is_heredoc(char *line);
+void	f_free_array(char **r);
 
 #endif

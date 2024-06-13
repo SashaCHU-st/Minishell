f4daf1545b05da8fad230ff773408f8ec1a8c6cf
Elena <yelenakras@gmail.com>
2024-06-13 11:40:12 +0300

    test0

--- builtins.c
+++ builtins.c
@@ -6,7 +6,7 @@
 /*   By: aheinane <aheinane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 10:12:15 by aheinane          #+#    #+#             */
-/*   Updated: 2024/06/06 08:50:05 by aheinane         ###   ########.fr       */
+/*   Updated: 2024/06/07 12:19:04 by aheinane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,21 @@ int main (int argc, char **argv, char **envp)
 {
 	t_built data;
 	data.envp = envp;
+	data.argc = argc;
+	data.argv = argv;
 	
 	if(argc == 1)
 		printf("");
 	else if(argc > 1 &&  ft_strncmp(argv[1], "echo", 5) == 0) //if found echo, nujno esche $ pro env
 		ft_echo(argc, argv);
-	else if (argc > 1 && ft_strncmp(argv[1], "pwd", 4) == 0)
-		ft_pwd(envp);
-	else if (argc > 1 && ft_strncmp(argv[1], "cd", 3) == 0)//// DOES NOT REALLY WORKS!!!!!!!
-		ft_cd(&data, argc, argv);
-	else if(argc > 1 && ft_strncmp(argv[1], "env", 4) == 0)
-		ft_env(&data);
-	else if(argc > 1 && ft_strncmp(argv[1], "export", 7) == 0)
-		ft_export(&data, argc,argv);
+	//else if (argc > 1 && ft_strncmp(argv[1], "pwd", 4) == 0)
+	//	ft_pwd();
+	//else if (argc > 1 && ft_strncmp(argv[1], "cd", 3) == 0)//// DOES NOT REALLY WORKS!!!!!!!
+	//	ft_cd(&data);
+	//else if(argc > 1 && ft_strncmp(argv[1], "env", 4) == 0)
+	//	ft_env(&data);
+	//else if(argc > 1 && ft_strncmp(argv[1], "export", 7) == 0)
+	//	ft_export(&data);
 	else if(argc > 1 && ft_strncmp(argv[1], "unset", 6) == 0)
 		ft_unset(&data, argc,argv);
 	return(0);

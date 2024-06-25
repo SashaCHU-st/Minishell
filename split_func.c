7743985e30a561ed5b1426cf629febb4ad69658b
Elena <yelenakras@gmail.com>
2024-06-25 21:03:43 +0300

    combine both structs for buildins and minishell, add split by word

--- split_func.c
+++ split_func.c
@@ -109,6 +109,8 @@ static char	**f_fill_array(char const *s, char c, unsigned int nb)
 	unsigned int	len;
 
 	array = allocate_array(nb);
+	if (!array)
+		return(NULL);
 	start = 0;
 	i = 0;
 	while (i < nb)
@@ -118,7 +120,10 @@ static char	**f_fill_array(char const *s, char c, unsigned int nb)
 			len = get_substring_length(s, start, c);
 			array[i] = ft_substr(s, start, len);
 			if (!array[i])
+			{
 				f_free_array(array);
+				return (NULL);
+			}
 			i++;
 			start += len;
 		}

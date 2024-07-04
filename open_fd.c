5a1d26bc90ded1a29306b9453cc3307649cf8ae5
Aleksandra Heinanen <aheinane@c1r2p3.hive.fi>
2024-07-04 16:54:00 +0300

    merged structs

--- open_fd.c
+++ open_fd.c
@@ -6,22 +6,27 @@
 /*   By: aheinane <aheinane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 15:46:25 by aheinane          #+#    #+#             */
-/*   Updated: 2024/07/02 15:11:30 by aheinane         ###   ########.fr       */
+/*   Updated: 2024/07/04 16:35:51 by aheinane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "builtins.h"
 
-int	open_fd_in(t_pipex *pipex, t_built *shell)
+int	open_fd_in(t_pipex *pipex, t_data *shell)
 {
-		printf("hello3\n");
-	if (access(shell->data.cmds->word_tok[0], F_OK | R_OK) == -1)
+	int i  = 0;
+	printf("hello3\n");
+	//printf("KUKU %s\n",shell->data.cmds[i].filenames[1]);
+	printf("KUKU %s\n",shell->cmds[i].filenames[0]);
+	if (access(shell->cmds->filenames[1], F_OK | R_OK) == -1)
 	{
+		printf("hello88888\n");
 		perror("No access for input");
 		return (1);
 	}
-	pipex->fd_in = open(shell->data.cmds->word_tok[0], O_RDONLY);
+	printf("hello4\n");
+	pipex->fd_in = open(shell->cmds->filenames[1], O_RDONLY);
 	if (pipex->fd_in == -1)
 	{
 		perror("Error in infile");
@@ -30,9 +35,10 @@ int	open_fd_in(t_pipex *pipex, t_built *shell)
 	return (0);
 }
 
-void	open_fd_out(t_pipex *pipex, t_built *shell, int number_of_inputs)
+//void	open_fd_out(t_pipex *pipex, t_data *shell, int number_of_inputs)
+void	open_fd_out(t_pipex *pipex, t_data *shell)
 {
-	pipex->fd_out = open(shell->data.cmds->word_tok[number_of_inputs -1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	pipex->fd_out = open(shell->cmds->filenames[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (pipex->fd_out == -1)
 	{
 		perror("Error in outfile");

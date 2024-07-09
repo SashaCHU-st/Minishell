dd53a1425e1f04a31eec6aa9df6abef2979f27c1
Aleksandra Heinanen <aheinane@c1r1p7.hive.fi>
2024-07-09 16:01:22 +0300

    open fd/out

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: aheinane <aheinane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 15:47:03 by aheinane          #+#    #+#             */
-/*   Updated: 2024/07/08 17:12:19 by aheinane         ###   ########.fr       */
+/*   Updated: 2024/07/09 16:00:06 by aheinane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,53 +45,80 @@ char	*path_for_commands(t_pipex *pipex, char **child_command)
 	}
 	return (NULL);
 }
+// void	check_filetype(t_pipex *pipex, t_cmd *cmd)
+// {
+// 	int i = 0;
 
-//void	creating_children( t_pipex *pipex, t_data *shell, int number_of_inputs)
-void	creating_children( t_pipex *pipex, t_data *shell)
-{
-	int		first_child = 0;
+// 	while (i < cmd->number_of_redir)
+// 	{
+// 		if (cmd->filetype[i] == IN)
+// 			open_fd_in(pipex, cmd->filetype[i], cmd->filenames[i]);
+// 		if (cmd->filetype[i] == OUT)
+// 			open_fd_out(pipex, cmd->filetype[i], cmd->filenames[i]);
+// 		i++;
+// 	}
+// 	printf("LOOP ENDED\n");
+// }
 
-	if(shell->cmds_count == 1)
-	{
-		printf("Hello1\n");
-		first_child = 0;
-		//check_permissions(shell);
-		if (open_fd_in(pipex, shell) == 1)
-			ft_putstr_fd(": No such file or directory\n", 2);
-		else
-		{
-			printf("Hello2\n");
-			first_child = fork();
-			printf("Hello3\n");
-			if (first_child < 0)
-				ft_error();
-			if (first_child == 0)
-				fun_first_child(shell, *pipex);
-		}
-	}
+// int	open_fd_in(t_pipex *pipex, int filetype, char *filename)
+// {
+// 	if(filetype == 1)
+// 	{
+// 		if (access(filename, F_OK | R_OK) == -1)
+// 		{
+// 			perror("No access for input");
+// 			return (1);
+// 		}
+// 		pipex->fd_in = open(filename, O_RDONLY);
+// 		if (pipex->fd_in == -1)
+// 		{
+// 			perror("Error in infile");
+// 			return (1);
+// 		}
+// 	}
 	
-	if(shell->cmds_count == 2)
+// 	return (0);
+// }
+
+// //void	open_fd_out(t_pipex *pipex, t_data *shell, int number_of_inputs)
+// void	open_fd_out(t_pipex *pipex, int filetype, char *filename)
+// {
+// 	if(filetype == 2)
+// 	{
+		
+// 	pipex->fd_out = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+// 	if (pipex->fd_out == -1)
+// 	{
+// 		perror("Error in outfile");
+// 		close(pipex->fd_in);
+// 		exit(EXIT_FAILURE);
+// 	}
+// 	}
+
+// }
+//void	creating_children( t_pipex *pipex, t_data *shell, int number_of_inputs)
+void creating_children(t_pipex *pipex, t_data *shell)
+{
+	int first_child = 0;
+
+	first_child = fork();
+	if (first_child < 0)
+		ft_error();
+	if (first_child == 0)
 	{
-		first_child = 0;
-		//check_permissions(shell);
-		if (open_fd_in(pipex, shell) == 1)
-			ft_putstr_fd(": No such file or directory\n", 2);
-		else
-		{
-			first_child = fork();
-			if (first_child < 0)
-				ft_error();
-			if (first_child == 0)
-				fun_first_child(shell, *pipex);
-		}
-		open_fd_out(pipex, shell);
-		pipex->second_child=0;
-		pipex->second_child = fork();
-		if (pipex->second_child  < 0)
-			ft_error();
-		if (pipex->second_child == 0)
-			fun_second_child(shell, *pipex);
+		check_filetype(pipex, &shell->cmds[0]);
+		fun_first_child(shell, *pipex);
 	}
+		// pipex->second_child = fork();
+		// if (pipex->second_child < 0)
+		// {
+		// 	ft_error();
+		// }
+		// if (pipex->second_child == 0)
+		// {
+		// 	check_filetype(pipex, &shell->cmds[1]);
+		// 	fun_second_child(shell, *pipex);
+		// }
 	close_and_wait(shell, pipex, first_child);
 }
 

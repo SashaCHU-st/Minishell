d8ef5a02069bbf92d73de9bdfd78dc93fd5d3054
Elena Polkhovski <epolkhov@c1r1p5.hive.fi>
2024-07-20 21:13:54 +0300

    fix expand

--- expand.c
+++ expand.c
@@ -23,14 +23,12 @@ static char	*receive_exit_status(t_data *shell)
 {
 	char	*status;
 
-	printf("exit status %d\n", shell->exit_status);
 	if (shell->exit_status > 255)
 		status = ft_itoa(WEXITSTATUS(shell->exit_status));
 	else
 		status = ft_itoa(shell->exit_status);
 	if (!status)
 		error_message(shell, "Malloc failed", 1);
-	printf("exit status %s\n", status);
 	return (status);
 }
 
@@ -42,7 +40,10 @@ static char	*get_expand(t_data *shell, char *line)
 
 	len = 0;
 	if (line[len] == '?')
+	{
+		printf("ststus\n");
 		return (receive_exit_status(shell));
+	}
 	while (!end_character(line[len]))
 		len++;
 	if (len == 0)
@@ -53,6 +54,7 @@ static char	*get_expand(t_data *shell, char *line)
 	ft_strncpy(env, line, len);
 	env[len] = '\0';
 	value = ft_getenv(shell, env);
+	printf("I received: %s \n", value);
 	free (env);
 	if (!value)
 		value = ft_strdup("");
@@ -64,25 +66,31 @@ static char	*expand_env(t_data *shell, char **line, int *i)
 	int		st;
 	char	*value;
 	char	*new_line;
-	int		new_line_len;
-
+	int		new_line_len = 0;
+	
 	st = (*i) + 1;
+	printf("value before expand: %s\n", *line);
+	printf("value before expand: %c\n", (*line)[st]);
 	value = get_expand(shell, &(*line)[st]);
+	printf("value: %s\n", value);
 	if (!value)
 		error_message(shell, "Expansion of env failed", 1);
+	shell->var_len = 0;
 	while (!end_character((*line)[st + shell->var_len]))
 		shell->var_len++;
 	new_line_len = ft_strlen(value) + ft_strlen(*line) - shell->var_len - 1;
-	new_line = (char *)malloc(sizeof(char) * new_line_len + 1);
+	if (new_line_len < 0)
+		new_line_len = 0;
+	new_line = (char *)malloc(sizeof(char) * (new_line_len + 1));
 	if (!new_line)
 	{
 		free(value);
 		error_message(shell, "Failed to malloc", 1);
 	}
 	ft_strncpy(new_line, *line, (*i));
-	ft_strncpy(new_line + (*i), value, ft_strlen(value));
-	ft_strncpy(new_line + (*i) + ft_strlen(value), *line + st + shell->var_len,
-		ft_strlen(*line) - st - shell->var_len);
+	new_line[(*i)] = '\0';
+	strcpy(new_line + (*i), value);
+    strcpy(new_line + (*i) + ft_strlen(value), *line + st + shell->var_len);
 	new_line[new_line_len] = '\0';
 	free(value);
 	return (new_line);
@@ -90,7 +98,7 @@ static char	*expand_env(t_data *shell, char **line, int *i)
 
 char	*expand_var(t_data *shell, char *line)
 {
-	int		j;
+	int	j;
 
 	j = 0;
 	while (line[j])
@@ -100,7 +108,7 @@ char	*expand_var(t_data *shell, char *line)
 		if (line[j + 1] && line[j] == '\'' && !shell->in_dquotes)
 			j = skip_quotes(line, j);
 		if (line[j] == '$' && line[j + 1] && line[j + 1] != ' '
-			&& line[j + 1] != '$')
+			&& line[j + 1] != '$' && (ft_isdigit(line[j + 1]) || ft_isalpha(line[j + 1]) || line[j + 1] == '?'))
 		{
 			shell->expanded_line = expand_env(shell, &line, &j);
 			if (!shell->expanded_line)
@@ -112,7 +120,8 @@ char	*expand_var(t_data *shell, char *line)
 			free(line);
 			line = shell->expanded_line;
 		}
-		j++;
+		else
+			j++;
 	}
 	return (line);
 }

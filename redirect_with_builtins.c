814fd8aaacb8817119ef9be26a7d62be25388120
Elena Polkhovski <epolkhov@c1r1p5.hive.fi>
2024-07-20 15:04:57 +0300

    fix running command function

--- redirect_with_builtins.c
+++ redirect_with_builtins.c
@@ -24,19 +24,28 @@ void	redirection_in_builtin(t_data *shell, t_pipex *pipex, int i)
 
 void	redirection_out_builtin(t_data *shel, t_pipex *pipex, int i)
 {
+	//printf("dididid\n");
 	shel->parent_out = dup(STDOUT_FILENO);
+	//printf("daaaaaaa\n");
 	open_fd_out(shel, pipex, shel->cmds->filetype[i], *shel->cmds->filenames);
-	check_filetype(shel, pipex, &shel->cmds[i]);
+	//check_filetype(shel, pipex, &shel->cmds[i]);
+	//printf("nuuuuut\n");
 	builtins(shel, &shel->cmds[i], i);
 	if (dup2(shel->parent_out, STDOUT_FILENO) < 0)
 		printf("dup2 \n");
+	//printf("come on\n");
 	close(shel->parent_out);
 }
 
 void	redirection_with_builtins(t_data *shell, t_pipex *pipex, int i)
 {
+	//printf("dididid\n");
 	if (shell->cmds->filetype[i] == OUT)
+	{
+		//printf("LLLLLL\n");
 		redirection_out_builtin(shell, pipex, i);
+		//printf("wwwwwwwwwwww\n");
+	}
 	if (shell->cmds->filetype[i] == IN)
 		redirection_in_builtin(shell, pipex, i);
 }

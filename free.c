ace6c308091d5ff702b26735539164769d5499df
Elena <yelenakras@gmail.com>
2024-07-23 23:34:47 +0300

    back to earlier version

--- free.c
+++ free.c
@@ -6,7 +6,7 @@
 /*   By: aheinane <aheinane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 15:46:00 by aheinane          #+#    #+#             */
-/*   Updated: 2024/07/23 20:42:05 by aheinane         ###   ########.fr       */
+/*   Updated: 2024/07/22 21:02:32 by aheinane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,8 @@
 
 void	free_fun(t_pipex *pipex)
 {
-	if (pipex->commands_path)
+	if (pipex->commands_path != NULL)
 		free_array(pipex->commands_path);
-	
 	close(STDIN_FILENO);
 	close(STDOUT_FILENO);
 	exit(1);
@@ -33,7 +32,6 @@ void	free_array(char **array)
 	while (array[i])
 	{
 		free(array[i]);
-		array[i] = NULL;
 		i++;
 	}
 	free (array);
@@ -55,39 +53,36 @@ void	f_free_array(char **str)
 	free(str);
 }
 
-static void	free_wtok(t_cmd *cmd)
+void	ft_error(void)
 {
-	int	i;
-
-	if (cmd->word_tok)
-	{
-		i = 0;
-		while (i < cmd->w_count)
-		{
-			if (cmd->word_tok[i])
-				free(cmd->word_tok[i]);
-			i++;
-		}
-		free(cmd->word_tok);
-	}
+	perror("Error fork()");
+	exit(1);
 }
 
-void	f_free_cmds(t_cmd *cmd)
+void	f_free_cmds(t_cmd *cmds, int cmds_count)
 {
-	int	i;
+	int		i;
 
-	free_wtok(cmd);
-	if (cmd->filenames)
+	i = 0;
+	while (i < cmds_count)
 	{
-		i = 0;
-		while (i < cmd->number_of_redir)
+		if (cmds[i].word_tok != NULL)
 		{
-			if (cmd->filenames[i])
-				free(cmd->filenames[i]);
-			i++;
+			free_array(cmds[i].word_tok);
+			cmds[i].word_tok = NULL;
 		}
-		free(cmd->filenames);
+		if (cmds[i].filetype != NULL)
+		{
+			free(cmds[i].filetype);
+			cmds[i].filetype = NULL;
+		}
+		if (cmds[i].filenames != NULL)
+		{
+			free_array(cmds[i].filenames);
+			cmds[i].filenames = NULL;
+		}
+		i++;
 	}
-	if (cmd->filetype)
-		free(cmd->filetype);
+	free(cmds);
+	cmds = NULL;
 }
